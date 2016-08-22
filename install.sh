40b3569a60e0bd483be825cf3c243067c049bdcd
KHMF Technologies <spilgit@users.noreply.github.com>
2016-08-22 11:18:20 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,117 @@
+     1	17:23	who
+     2	17:23	whoami
+     3	17:23	last
+     4	17:27	cd /usr/ports/security/sudo
+     5	17:28	cd /usr/ports/security
+     6	17:28	cd /usr/ports
+     7	17:28	cd /usr
+     8	17:28	pkg install lynx
+     9	17:29	lynx
+    10	17:29	fdisk
+    11	17:29	pkg install ntpd
+    12	17:30	pkg install apache24
+    13	17:33	service apache24 start
+    14	17:33	pkg install nano
+    15	17:33	nano /etc/rc.conf
+    16	17:34	service apache24 start
+    17	17:34	service spache24 status
+    18	17:34	service apache24 status
+    19	17:35	ifconfig
+    20	17:35	service apache24 configtest
+    21	17:36	pkg install mod_ssl mod_perl mod_php
+    22	17:36	cd /usr/ports/www/apache24
+    23	17:37	cd /usr/ports/www
+    24	17:37	cd /usr
+    25	17:37	ls
+    26	17:37	pkg install wordpress
+    27	17:41	pkg search -D mysql
+    28	17:41	pkg search -D mysql56-server
+    29	17:42	pkg install mysql56-server
+    30	17:43	pkg install mod_php56 php56-mysql php56-mysqli
+    31	17:43	cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+    32	17:43	rehash
+    33	17:43	pkg search php56
+    34	17:44	pkg search -f php56-calendar
+    35	17:44	pkg install php56-calendar
+    36	17:44	nano /usr/local/etc/apache24/Includes/php.conf
+    37	17:45	find / -name php.conf
+    38	17:45	nano /usr/local/etc/php.conf
+    39	17:46	service apache24 restart
+    40	17:47	cd /home
+    41	17:47	mkdir /home
+    42	17:47	cd /home
+    43	17:47	ls
+    44	17:47	history > 05182016history.sh
+     1	17:21	top
+     2	17:21	ping yahoo.ca
+     3	17:21	ping -c 4 google.com
+     4	17:22	service sshd status
+     5	17:22	ifconfig
+     6	17:23	who
+     7	17:23	whoami
+     8	17:23	last
+     9	17:27	cd /usr/ports/security/sudo
+    10	17:28	cd /usr/ports/security
+    11	17:28	cd /usr/ports
+    12	17:28	cd /usr
+    13	17:28	pkg install lynx
+    14	17:29	lynx
+    15	17:29	fdisk
+    16	17:29	pkg install ntpd
+    17	17:30	pkg install apache24
+    18	17:33	service apache24 start
+    19	17:33	pkg install nano
+    20	17:33	nano /etc/rc.conf
+    21	17:34	service apache24 start
+    22	17:34	service spache24 status
+    23	17:34	service apache24 status
+    24	17:35	ifconfig
+    25	17:35	service apache24 configtest
+    26	17:36	pkg install mod_ssl mod_perl mod_php
+    27	17:36	cd /usr/ports/www/apache24
+    28	17:37	cd /usr/ports/www
+    29	17:37	cd /usr
+    30	17:37	ls
+    31	17:37	pkg install wordpress
+    32	17:41	pkg search -D mysql
+    33	17:41	pkg search -D mysql56-server
+    34	17:42	pkg install mysql56-server
+    35	17:43	pkg install mod_php56 php56-mysql php56-mysqli
+    36	17:43	cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+    37	17:43	rehash
+    38	17:43	pkg search php56
+    39	17:44	pkg search -f php56-calendar
+    40	17:44	pkg install php56-calendar
+    41	17:44	nano /usr/local/etc/apache24/Includes/php.conf
+    42	17:45	find / -name php.conf
+    43	17:45	nano /usr/local/etc/php.conf
+    44	17:46	service apache24 restart
+    45	17:47	cd /home
+    46	17:47	mkdir /home
+    47	17:47	cd /home
+    48	17:47	ls
+    49	17:47	history > 05182016history.sh
+    50	17:47	ls
+    51	17:47	nano 05182016history.sh
+    52	17:49	nano /etc/rc.conf
+    53	17:51	nano /etc/rc.conf
+    54	17:52	service pf start
+    55	17:52	pkg install pf pflog
+    56	17:52	pfctl
+    57	17:53	nano /etc/pf.conf
+    58	17:55	exit
+    59	17:55	exit
+    60	22:12	adduser
+    61	22:13	adduser
+    62	22:15	nano /etc/sudoers
+    63	22:16	find / -name sudoers
+    64	22:18	pkg install subversion
+    65	22:19	svn checkout https://svn.FreeBSD.org/ports/head /usr/ports
+    66	23:05	svn update /usr/ports
+    67	23:13	cd /usr/ports/
+    68	23:14	ls
+    69	23:14	history
+    70	23:16	cd .home
+    71	23:16	cd /home
+    72	23:16	ls
+    73	23:17	history >> 05182016history.sh
